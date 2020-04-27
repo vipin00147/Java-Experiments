88b79ff6486d47bd3e2e365161caf1212f5f7cd4
vipin00147 <61090829+vipin00147@users.noreply.github.com>
2020-04-27 13:29:27 +0530

    Create Show.java

---
+++ Exp 9(C)/Show.java
@@ -0,0 +1,10 @@
+import details.Details;
+public class Show
+{
+    public static void main(String args[])
+    {
+        Details obj = new Details();
+        obj.Branch(19,"Vipin","BCA");
+        System.out.println(obj.output());
+    }
+}
